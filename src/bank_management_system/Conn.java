f2ed19f298265bd8d2eb41ee8428593aa860e95e
Dega Chanikya <chanikyadega@gmail.com>
2026-06-12 15:04:56 +0530

    Initial Bank Management System Project

---
+++ src/bank_management_system/Conn.java
@@ -0,0 +1,20 @@
+package bank_management_system;
+
+import java.sql.*;
+
+public class Conn {
+    
+    Connection c;
+    Statement s;
+    public Conn() {
+        try {
+            c = DriverManager.getConnection("jdbc:mysql://localhost:3306/bankmanagementsystem", "root", "Dega@541");
+            s = c.createStatement();
+            
+        } catch (Exception e) {
+            System.out.println(e);
+        }
+    }
+
+
+}
